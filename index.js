78e6d3ca1bb64e6bf2fb4adf6d1fd3e539277496
Jegedeglory <Jegsboy007@gmail.com>
2023-06-12 15:47:41 +0100

    functions for fullname

---
+++ index.js
@@ -0,0 +1,57 @@
+/*const firstName = 'Glory';
+
+const lastName = 'Jegede';
+let address = "mainstreet";
+address = "firststreet";
+
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+
+
+
+
+
+const street = 'Elesare';
+const country = 'Nigeria';
+const fullMailingAddress = 'street' + ' ' + 'country';
+console.log(street);
+console.log(country);
+console.log(fullMailingAddress);
+
+
+
+const numbr = 434;
+const numbr2 = 433;
+console.log(typeof fullMailingAddress);
+
+const result = numbr2 < numbr;
+
+//if (result) {
+ //    console.log('value is true');
+//}
+
+result ?  console.log('value is true'):console.log('value is false');*/
+
+
+/*const names = [ 'Jegede', 'Glory', 'Gbolahan'];
+const SurName = 'Jegede';
+let newName = [];
+
+for (var t = 0; t < names.length; t++);{
+    console.log(t);
+    console.log(`${names[t]} ${SurName}`);
+
+    newName.push(names[t])
+}
+
+console.log(names);
+console.log(newName);*/
+
+
+function fullName(names) {
+    console.log( 'My name is ' + names)
+}
+fullName('Jegede');
+fullName('Glory');
+fullName('Gbolahan');
